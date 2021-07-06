a24b40d0b9737b3e3713ee0346e473ed86870b67
SyedMHammad <smhammad163@gmail.com>
2021-07-07 03:11:48 +0500

    refactored the code and archived the previous script
    refactored the code, changed the type of password input to password from text and saved the previous version of script as backup

--- p01/script.js
+++ p01/script.js
@@ -2,7 +2,7 @@ const form=document.getElementById("form");
 const email=document.getElementById("email");
 const username=document.getElementById("username");
 const password=document.getElementById("Password");
-const password2=document.getElementById("Password2");
+const password2=document.getElementById("Confirm Password");
 function showfailure(input,message)
 {
     const formcontrol=input.parentElement;
@@ -19,46 +19,65 @@ function isValid(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+function getFieldId(input)
+{
+   return input.id.charAt(0).toUpperCase()+input.id.slice(1);
+}
+function check(inputArray){
+    inputArray.forEach(function(input)
+    {
+        if(input.value ==='')
+        {
+            showfailure(input,`${getFieldId(input)} cannot be empty`);
+        }
+        else
+        {
+            showsuccess(input);
+        }
+    });
+}
+function checkLength(input,min,max)
+{
+    if(input.value.length < min)
+    {
+        showfailure(input,`${getFieldId(input)} length should be between ${min} and ${max}`);
+    }
+    else if(input.value.length > max)
+    {
+        showfailure(input,`${getFieldId(input)} should be between ${min} and ${max}`);
+    }
+    else
+    {
+        showsuccess(input);
+    }
+}
+function checkEmail(input)
+{
+    if(!isValid(input.value))
+    {
+        showfailure(input,"Email is invalid");
+    }
+    else{
+        showsuccess(input);
+    }
+}
+function checkpassword(input1,input2)
+{
+    if(input1.value!=input2.value)
+    {
+        showfailure(input,"Passwords did not match");
+    }
+    else
+    {
+        showsuccess(input2);
+    }
+}
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    if(username.value === ''){
-        showfailure(username,'Username cannot be empty');
-    }
-    else
-    {
-        showsuccess(username);
-    }
-    if(email.value === ''){
-        showfailure(email,'Email cannot be empty');
-    }
-    else if(!isValid(email.value))
-    {
-        showfailure(email,'Enter valid email');
-    }
-    else
-    {
-        showsuccess(email);
-    }
-
-    if(password.value === ''){
-        showfailure(password,'Password cannot be empty');
-    }
-    else
-    {
-        showsuccess(password);
-    }
-    if(password2.value === ''){
-        showfailure(password2,'Passwords did not match');
-    }
-    else if(password2.value != password.value)
-    {
-        showfailure(password2,'Passwords did not match');
-    }
-    else
-    {
-        showsuccess(password2);
-    }
     
-    
-    
+    checkEmail(email);
+    check([username,email,password,password2]);
+    checkpassword(password,password2);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
 })
